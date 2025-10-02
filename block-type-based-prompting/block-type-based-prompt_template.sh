fa0bf1fe0208e57c785098f2f68bcdb4bc684698
qlbdsc <smudsc@163.com>
2025-10-02 20:10:49 +0800

    Update block-type-based-prompt_template.sh

--- block-type-based-prompting/block-type-based-prompt_template.sh
+++ block-type-based-prompting/block-type-based-prompt_template.sh
@@ -10,31 +10,31 @@ A prerequisite is to not consider inserting a new log before or after the "throw
 **Overall Task:**  
 You need to insert a log into the target method.
 **Step-by-Step Instructions:**
-0. **Locate Branching Block$type1: **
-   - Identify the start and end lines of Branching Block$type1 based on the annotations provided in the target method. 
+0. **Locate Branching Block$BranchSequenceNumber: **
+   - Identify the start and end lines of Branching Block$BranchSequenceNumber based on the annotations provided in the target method. 
    - Use these annotations to determine the exact scope of the block before proceeding to the next steps.
 1. **Identify Inner Blocks:**
-   - Locate the inner blocks that are nested within Branching Block$type1 itself (e.g., inner if, try-catch, loop, etc. inside it).  
-   - Do not insert a log inside inner blocks nested within Branching Block$type1.
-Note: This restriction only applies to inner blocks within Branching Block$type1.
-Even if Branching Block$type1 itself is nested inside some other outer block, you may still consider inserting a log for it — as long as the log is placed outside its own inner blocks.
+   - Locate the inner blocks that are nested within Branching Block$BranchSequenceNumber itself (e.g., inner if, try-catch, loop, etc. inside it).  
+   - Do not insert a log inside inner blocks nested within Branching Block$BranchSequenceNumber.
+Note: This restriction only applies to inner blocks within Branching Block$BranchSequenceNumber.
+Even if Branching Block$BranchSequenceNumber itself is nested inside some other outer block, you may still consider inserting a log for it — as long as the log is placed outside its own inner blocks.
 2. **Determine Log Requirement:**  
-   - Determine whether a log need to be inserted for Branching Block$type1.
+   - Determine whether a log need to be inserted for Branching Block$BranchSequenceNumber.
    - Base your decision on the following criteria.
 3. **Choose Log Insertion Positions:**  
    - If a log is required, select the appropriate positions for insertion.
-   - If both the if block and else block of  Branching Block$type1 require a log, If  you think both the block and else block of Branching Block$type1 require a log, you may provide two line numbers separated by a comma: one for the if block and one for the else block.
+   - If both the if block and else block of  Branching Block$BranchSequenceNumber require a log, If  you think both the block and else block of Branching Block$BranchSequenceNumber require a log, you may provide two line numbers separated by a comma: one for the if block and one for the else block.
 4. **Handle No Log Requirement:**  
-   - If no log is required for Branching Block$type1, respond with "N/A" for position choice and quality score.
+   - If no log is required for Branching Block$SequenceNumber, respond with "N/A" for position choice and quality score.
 
-When you determine whether a log need to be inserted for Branching Block$type1, please evaluate according to the following standard:
+When you determine whether a log need to be inserted for Branching Block$BranchSequenceNumber, please evaluate according to the following standard:
 Logs should capture events that directly influence the program‘s behavior and outcomes, such as critical resource management, exception handling, and key method branches. Special attention should be given to logs placed after failure conditions or when the behavior has a direct effect on program execution (e.g., deletion failures).
 Redundant logs refer to the repetition of identical or similar information, which can cause log files to become too large and difficult to analyze. Additionally, frequent logging, especially in high-frequency operations, can introduce performance overhead and affect system responsiveness. This is particularly important for logs capturing the same event, such as re-logging exceptions that have already been logged at a higher level.
 If a log is frequently recorded, it could have performance implications, especially if the event occurs in high-frequency operations like loops.
 
 Let us think step by step,
 The target method is blow:
-$funccomment 
+$function_with_annotation
 Let’s think step by step. First, 
 $methodcode 
 Second, the succeeding and preceding logs are:
@@ -67,16 +67,16 @@ Complete following instruction while staying in strict accordance with the natur
 You need to insert logs into the target method.
 **Step-by-Step Instructions:**  
 1. **Identify Inner Blocks:**
-   - Identify the scope of Try-Catch Block$type2.
-   - An inner block refers to a code block whose scope is completely enclosed within the scope of Try-Catch Block$type2. Please interpret and respond based on this definition.
-   - Locate the inner blocks within the Try-Catch Block$type2. Exclude the Try and Catch blocks of Try-Catch Block$type2 itself.
-   - Determine whether the scope of the inner block is enclosed within the scope of Try-Catch Block$type2. If not, just respond with "N/A".
+   - Identify the scope of Try-Catch Block$TrycatchSequenceNumber.
+   - An inner block refers to a code block whose scope is completely enclosed within the scope of Try-Catch Block$TrycatchSequenceNumber. Please interpret and respond based on this definition.
+   - Locate the inner blocks within the Try-Catch Block$TrycatchSequenceNumber. Exclude the Try and Catch blocks of Try-Catch Block$TrycatchSequenceNumber itself.
+   - Determine whether the scope of the inner block is enclosed within the scope of Try-Catch Block$TrycatchSequenceNumber. If not, just respond with "N/A".
    - Do not insert logs within the scope of these inner blocks.
    - If do not have inner blocks, just respond with "N/A".
 2. **Determine Log Requirement:**  
-   - Assess whether logs need to be inserted for Try-Catch Block$type2.  
+   - Assess whether logs need to be inserted for Try-Catch Block$TrycatchSequenceNumber.  
    - Specify how many logs are required.
-   - If you think Try-Catch Block$type2 do not need a log, just respond with N/A.
+   - If you think Try-Catch Block$TrycatchSequenceNumber do not need a log, just respond with N/A.
 3. **Choose Log Insertion Positions:**  
    - If a log is required in catch block, select the appropriate positions for insertion.
    - If a try-catch block contains multiple catch blocks, evaluate each one individually to decide if a log should be added.
@@ -89,22 +89,20 @@ You need to insert logs into the target method.
 6. **Predict the complete log in the position you choosed.**
 
 The target method is blow：
-$funccomment 
+$function_with_annotation 
 Let’s think step by step. First, 
 $methodcode 
-Second, 
-$funcsmu
-Thirdly,the variable list is:
-$VarList
+Second, the succeeding and preceding logs are:
+$logslice
 
 Please provide answers without any surrounding symbols or formatting. For example, respond with '9' instead of '[9]' or '(9)' to ensure the answer is plain and unembellished.
 Please strictly follow the specified output format below and do not give me other information:
-Block Name: [Name of the block]
-Determine Whether Log is Needed: [Yes/ N/A]
-Line Number of Position Choice: [Line number]
-Chain-of-Thought: [Explain your reasoning]
-Quality Score of the Position Choice: [Score, 1-10]
-Sub-Score for the Quality Score: [Explanation for the sub-score]
+Blcok name: #
+Determine Whether Log is Needed: #
+Line number of position choice:#
+Chain-of-Thought:#
+quality score: #
+Sub-Score for the quality score: #
 Do not add any extra text, explanations, or symbols beyond the required output.
 
 EOF
@@ -123,23 +121,21 @@ Complete following instruction while staying in strict accordance with the natur
 You need to insert only one log into the target method.
 **Step-by-Step Instructions:**  
 1. **Identify Inner Blocks:**
-    - Locate the inner blocks within Looping Block$type3.
+    - Locate the inner blocks within Looping Block$LoopSequenceNumber.
     - Do not insert logs within the scope of these inner blocks.
 2. **Determine Log Requirement:**  
-   - Assess whether a log needs to be inserted for Looping Block$type3. If no log is required for Looping Block$type3, respond with N/A for all answers.
+   - Assess whether a log needs to be inserted for Looping Block$type3. If no log is required for Looping Block$LoopSequenceNumber, respond with N/A for all answers.
 3. **Choose a Log Insertion Position:**  
    - If a log is required in Looping block, select the appropriate positions for insertion.
 4. **Handle No Log Requirement:**  
-   - If no log is required for Looping Block$type3, respond with N/A for all answers.
+   - If no log is required for Looping Block$LoopSequenceNumber, respond with N/A for all answers.
 
 The target method is blow：
-$funccomment 
+$function_with_annotation 
 Let’s think step by step. First, 
 $methodcode 
-Second, 
-$funcsmu
-Thirdly,the variable list is:
-$VarList
+Second, the succeeding and preceding logs are:
+$logslice
 
 Please provide answers without any surrounding symbols or formatting. For example, respond with '9' instead of '[9]' or '(9)' to ensure the answer is plain and unembellished.
 Please strictly follow the specified output format below and do not give me other information:
@@ -166,10 +162,10 @@ Complete following instruction while staying in strict accordance with the natur
 You need to insert only one log into the target method.
 **Step-by-Step Instructions:**
 1. **Identify Inner Blocks:**
-- Locate the inner blocks within Method Declaration Block$type4.
+- Locate the inner blocks within Method Declaration Block$MDSequenceNumber.
 - Do not insert logs within the scope of these inner blocks.
 2. **Determine Log Requirement:**
-- Assess whether a log need to be inserted for Method Declaration Block$type4.
+- Assess whether a log need to be inserted for Method Declaration Block$MDSequenceNumber.
 3. **Choose a Log Insertion Position:**
 - Understand the function of target method.
 - Determine which line of code is truly responsible for the function of this method.
@@ -180,18 +176,16 @@ You need to insert only one log into the target method.
 - If the target method contains code for recording time, you also need to select a appropriate position to insert the log that complements the timing functionality.
 5. **Log placement for additional logs: **
 - If additional log is necessary, you also need to choose only one appropriate position for insertion. As a general rule, it is preferable to place logs near the end of the method. Specifically, if the method contains a return statement, the log should be placed immediately before it. If there is no return statement, the log should be placed near the end of the method body.
-- If additional log is unnecessary , no log positions need to be provided.
+- If additional log is unnecessary, no log positions need to be provided.
 6. **Handle No Log Requirement:**
-- If no log is required for Method Declaration Block$type4, respond with N/A.
+- If no log is required for Method Declaration Block$MDSequenceNumber, respond with N/A.
 
 The target method is blow：
-$funccomment 
+$function_with_annotation 
 Let’s think step by step. First, 
 $methodcode 
-Second, 
-$funcsmu
-Thirdly,the variable list is:
-$VarList
+Second, the succeeding and preceding logs are:
+$logslice
 
 Please provide answers without any surrounding symbols or formatting. For example, respond with '9' instead of '[9]' or '(9)' to ensure the answer is plain and unembellished.
 Please strictly follow the specified output format below and do not give me other information:
